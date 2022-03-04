fd059ecd5af90c2d58b22121b9b0ea48546999cc
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-03-03 22:47:44 -0800

    add App and NavTabs pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 
-import './App.css';
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+// import AboutMe from './pages/AboutMe';
+// import Portfolio from './pages/Portfolio';
+// import Contact from './pages/Contact';
+// import Resume from './pages/Resume';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("");
+
+  // const renderPage = () => {
+  //   if (currentPage === 'About Me') {
+  //     return <AboutMe />;
+  //   }
+  //   if (currentPage === 'Portfolio') {
+  //     return <Portfolio />;
+  //   }
+  //   if (currentPage === 'Contact') {
+  //     return <Contact />;
+  //   }
+  //   return <Resume />;
+  // };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* {renderPage()} */}
     </div>
   );
 }
