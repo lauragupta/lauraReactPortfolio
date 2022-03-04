1faf6dd1bd9ca6ecbac281793c4912f8f88fed4f
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-03-03 23:26:23 -0800

    add a basic About me page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 
 import React, { useState } from 'react';
-import NavTabs from './NavTabs';
-// import AboutMe from './pages/AboutMe';
-// import Portfolio from './pages/Portfolio';
-// import Contact from './pages/Contact';
-// import Resume from './pages/Resume';
+import NavTabs from './Components/NavTabs';
+import AboutMe from './Components/Pages/AboutMe';
+// import Portfolio from './Components/Pages/Portfolio';
+// import Contact from './Components/Pages/Contact';
+// import Resume from './Components/Pages/Resume';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("");
+  const [currentPage, setCurrentPage] = useState('About Me');
 
-  // const renderPage = () => {
-  //   if (currentPage === 'About Me') {
-  //     return <AboutMe />;
-  //   }
+  const renderPage = () => {
+    if (currentPage === 'About Me') {
+      return <AboutMe />;
+    }
   //   if (currentPage === 'Portfolio') {
   //     return <Portfolio />;
   //   }
@@ -20,16 +20,16 @@ function App() {
   //     return <Contact />;
   //   }
   //   return <Resume />;
-  // };
+  };
 
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div>
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* {renderPage()} */}
+      {renderPage()}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Pages/AboutMe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+        <p>
+          I am a full stack web developer. One of my core values is to always be learning new things. I also really enjoy helping people and teaching/mentoring others. I have done a lot of independent work as well as groupwork and paraprograming.  
+        </p>
+        <p>
+          I have a lot of work experience and have a history of staying with a company and advancing within the company. 
+        </p>
+        <p>
+          I love to travel and meet new people. I am interested in the environment, personal finance, real estate, health and fitness, writing, and mental health. I also love playing games, from board games to videogames, sports, and solving puzzles. 
+        </p>
+    </div>
+  );
+}
