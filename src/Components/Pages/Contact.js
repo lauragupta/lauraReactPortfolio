7176e8e2e75d5377970681fd75a674eafe42445b
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-03-06 15:02:18 -0800

    add error message for leaving Contact form field and not entering something

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Contact() {
+  const [errorName, setErrorName] = useState(false);
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  function onblurName(event) {
+    setErrorName(event.target.value === "") 
+  }
+
+  function onblurEmail(event) {
+    setErrorEmail(event.target.value === "") 
+  }
+  
+  function onblurMessage(event) {
+    setErrorMessage(event.target.value === "") 
+  }
+
   return (
-    <div class="column col-8 offset-1">
+    <div className="column col-8 offset-1">
       <h1>Contact Me</h1>
-      <form class="form contact-form">
-        <div class="form-group">
+      <form className="form contact-form">
+        <div className="form-group">
             <label for="contactName">name:</label>
-            <input class="form-control" type="text" id="signUpName" />
+            <input className="form-control" type="text" id="signUpName" onBlur={(event) => onblurName(event)}/>
+            {errorName && (
+              <div className="">
+                Please provide a valid name.
+              </div>
+            )}
         </div>
-        <div class="form-group">
+        <div className="form-group">
             <label for="contactEmail">email:</label>
-            <input class="form-control" type="email" id="contactEmail" />
+            <input className="form-control" type="email" id="contactEmail" onBlur={(event) => onblurEmail(event)} />
+            {errorEmail && (
+              <div className="">
+                Please provide a valid email address.
+              </div>
+            )}
         </div>
-        <div class="form-group">
+        <div className="form-group">
             <label for="message">Message</label>
-            <textarea class="form-control" type="text" id="message" />
+            <textarea className="form-control" type="text" id="message" onBlur={(event) => onblurMessage(event)} />
+            {errorMessage && (
+              <div className="">
+                Please provide a message.
+              </div>
+            )}
         </div>
-        <div class="form-group">
-            <button class="btn btn-primary" type="submit">Submit</button>
+        <div className="form-group">
+            <button className="btn btn-primary" type="submit">Submit</button>
         </div>
       </form>
     </div>
