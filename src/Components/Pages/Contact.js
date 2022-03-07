56c81c9ec6be368dcac7a15d00704563dca07bdd
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-03-07 01:27:51 -0800

    layout and styles on the contact form/page

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Contact.css'
 
 export default function Contact() {
   const [errorName, setErrorName] = useState(false);
@@ -25,46 +26,48 @@ export default function Contact() {
   }
 
   return (
-    <div>
-      <div className="column col-10 col-md-5 offset-1">
-        <h1>Contact Me</h1>
-        <form className="form contact-form">
-          <div className="form-group">
-              <label htmlFor="contactName">name:</label>
-              <input className="form-control" type="text" onBlur={(event) => onblurName(event)}/>
-              {errorName && (
-                <div className="formErr">
-                  Please provide a valid name.
-                </div>
-              )}
-          </div>
-          <div className="form-group">
-              <label htmlFor="contactEmail">email:</label>
-              <input className="form-control" type="email" onBlur={(event) => onblurEmail(event)} />
-              {errorEmail && (
-                <div className="formErr">
-                  Please provide a valid email address.
-                </div>
-              )}
-          </div>
-          <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea className="form-control" type="text" id="message" onBlur={(event) => onblurMessage(event)} />
-              {errorMessage && (
-                <div className="formErr">
-                  Please provide a message.
-                </div>
-              )}
-          </div>
-          <div className="form-group">
-              <button className="btn btn-primary" type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-      <div className="column col-11 col-md-5 main-info m-3 p-3">
-          <h5 className="main-info">Or You can contact me at...</h5>
-          <h6 className="main-info">codinglauragupta@gmail.com</h6>
-          <h6 className="main-info">‪(408)479-4045‬</h6>
+    <div className='container'>
+      <div className="row">
+        <h1 className='column offset-1'>Contact Me</h1>
+        <div className='column col-10 col-md-5 offset-1'>
+          <form className="form contact-form">
+            <div className="form-group">
+                <label htmlFor="contactName">name:</label>
+                <input className="form-control" type="text" onBlur={(event) => onblurName(event)}/>
+                {errorName && (
+                  <div className="formErr">
+                    Please provide a valid name.
+                  </div>
+                )}
+            </div>
+            <div className="form-group">
+                <label htmlFor="contactEmail">email:</label>
+                <input className="form-control" type="email" onBlur={(event) => onblurEmail(event)} />
+                {errorEmail && (
+                  <div className="formErr">
+                    Please provide a valid email address.
+                  </div>
+                )}
+            </div>
+            <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea className="form-control" type="text" id="message" onBlur={(event) => onblurMessage(event)} />
+                {errorMessage && (
+                  <div className="formErr">
+                    Please provide a message.
+                  </div>
+                )}
+            </div>
+            <div className="form-group">
+                <button className="contactSubmit btn btn-primary" type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+        <div className="otherContact column col-10 col-md-5 offset-1">
+            <h5 className="main-info">Or You can contact me at...</h5>
+            <h6 className="main-info">codinglauragupta@gmail.com</h6>
+            <h6 className="main-info">‪(408)479-4045‬</h6>
+        </div>
       </div>
     </div>
   );
