4cbda559ddb5cc899f1e7a39d441454ba2698c71
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-03-06 18:06:01 -0800

    add email validation to Contact form

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -10,48 +10,62 @@ export default function Contact() {
   }
 
   function onblurEmail(event) {
-    setErrorEmail(event.target.value === "") 
+    const isEmptyString = event.target.value === "";
+    const isValidEmail = validateEmail(event.target.value)
+    setErrorEmail(isEmptyString || !isValidEmail);
   }
   
   function onblurMessage(event) {
     setErrorMessage(event.target.value === "") 
   }
 
+  function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
   return (
-    <div className="column col-8 offset-1">
-      <h1>Contact Me</h1>
-      <form className="form contact-form">
-        <div className="form-group">
-            <label for="contactName">name:</label>
-            <input className="form-control" type="text" id="signUpName" onBlur={(event) => onblurName(event)}/>
-            {errorName && (
-              <div className="">
-                Please provide a valid name.
-              </div>
-            )}
-        </div>
-        <div className="form-group">
-            <label for="contactEmail">email:</label>
-            <input className="form-control" type="email" id="contactEmail" onBlur={(event) => onblurEmail(event)} />
-            {errorEmail && (
-              <div className="">
-                Please provide a valid email address.
-              </div>
-            )}
-        </div>
-        <div className="form-group">
-            <label for="message">Message</label>
-            <textarea className="form-control" type="text" id="message" onBlur={(event) => onblurMessage(event)} />
-            {errorMessage && (
-              <div className="">
-                Please provide a message.
-              </div>
-            )}
-        </div>
-        <div className="form-group">
-            <button className="btn btn-primary" type="submit">Submit</button>
-        </div>
-      </form>
+    <div>
+      <div className="column col-10 col-md-5 offset-1">
+        <h1>Contact Me</h1>
+        <form className="form contact-form">
+          <div className="form-group">
+              <label for="contactName">name:</label>
+              <input className="form-control" type="text" onBlur={(event) => onblurName(event)}/>
+              {errorName && (
+                <div className="formErr">
+                  Please provide a valid name.
+                </div>
+              )}
+          </div>
+          <div className="form-group">
+              <label for="contactEmail">email:</label>
+              <input className="form-control" type="email" onBlur={(event) => onblurEmail(event)} />
+              {errorEmail && (
+                <div className="formErr">
+                  Please provide a valid email address.
+                </div>
+              )}
+          </div>
+          <div className="form-group">
+              <label for="message">Message</label>
+              <textarea className="form-control" type="text" id="message" onBlur={(event) => onblurMessage(event)} />
+              {errorMessage && (
+                <div className="formErr">
+                  Please provide a message.
+                </div>
+              )}
+          </div>
+          <div className="form-group">
+              <button className="btn btn-primary" type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+      <div className="column col-11 col-md-5 main-info m-3 p-3">
+          <h5 className="main-info">Or You can contact me at...</h5>
+          <h6 className="main-info">codinglauragupta@gmail.com</h6>
+          <h6 className="main-info">‪(408)479-4045‬</h6>
+      </div>
     </div>
   );
 }
